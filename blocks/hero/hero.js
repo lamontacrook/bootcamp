30dda2c494f178d440798cbba015ddc3c54905ff
Lamont Crook <lamont@adobe.com>
2023-06-07 11:00:08 -0600

    design updates

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,3 +1,27 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+function getMetadata(name, doc) {
+  const attr = name && name.includes(':') ? 'property' : 'name';
+  const meta = [...doc.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
+  return meta || '';
+}
+
+/**
+ * Loads a fragment.
+ * @param {string} path The path to the fragment
+ * @returns {Document} The document
+ */
+async function loadFragment(path) {
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(path);
+    if (resp.ok) {
+      const parser = new DOMParser();
+      return parser.parseFromString(await resp.text(), 'text/html');
+    }
+  }
+  return null;
+}
+
 export default async function decorate(block) {
   const content = block.querySelectorAll('.hero.fullscreen>div');
 
@@ -19,5 +43,33 @@ export default async function decorate(block) {
 
     block.appendChild(contentDiv);
     picture.parentNode.classList.add('image');
+  }
+
+  const reference = block.querySelector('.hero.featured a');
+  console.log(reference);
+  if(reference) {
+    console.log(reference.getAttribute('href'));
+    const doc = await loadFragment(reference.getAttribute('href'));
+    const title = getMetadata('og:title', doc);
+    const desc = getMetadata('og:description', doc);
+    const image = createOptimizedPicture(doc.querySelector('img').src);
+
+    const h3 = document.createElement('h3');
+    h3.textContent = title;
+
+    const p = document.createElement('p');
+    p.textContent = desc;
+
+    const contentDiv = reference.parentElement.parentElement;
+    contentDiv.classList.add('content');
+
+    contentDiv.removeChild(reference.parentElement);
+    contentDiv.appendChild(h3);
+    contentDiv.appendChild(p);
+
+    const float = document.createElement('div');
+    float.appendChild(contentDiv);
+    float.appendChild(image);
+    block.appendChild(float);
   }
 }
