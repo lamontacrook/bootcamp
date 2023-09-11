f5784685e978070740704bfa646f442f1df582f8
Lamont Crook <lamont@adobe.com>
2023-09-11 11:36:19 -0600

    update footer

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,16 +1,20 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
+  // const cfg = readBlockConfig(block);
   block.textContent = '';
 
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta).pathname : '/footer';
+  const resp = await fetch(`${footerPath}.plain.html`);
+
   // fetch footer content
-  const footerPath = cfg.footer || '/footer';
-  const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
+  // const footerPath = cfg.footer || '/footer';
+  // const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
     const html = await resp.text();
