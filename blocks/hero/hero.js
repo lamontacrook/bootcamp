580859a900123498d14d4fcbd090bfd9b9e5c638
Lamont Crook <lamont@adobe.com>
2023-11-05 08:33:13 -0700

    updates

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,21 +1,27 @@
 export default async function decorate(block) {
-  const content = block.querySelectorAll('.hero.fullscreen>div');
+  const content = block.querySelectorAll('.hero>div');
+  let picture = '';
 
-  if (Object.keys(content).length > 0) {
-    let picture = '';
-    const contentDiv = document.createElement('div');
-    contentDiv.classList.add('content');
+  /**
+   * create a div for title and pre-title
+   */
+  const contentDiv = document.createElement('div');
+  contentDiv.classList.add('content');
 
-    [...content].forEach((div) => {
-      if (div.querySelector(':has(picture')) picture = div.querySelector(':has(picture');
-      else contentDiv.appendChild(div);
-    });
+  [...content].forEach((div) => {
+    /**
+     * check for picture tag
+     */
+    if (div.querySelector(':has(picture')) picture = div.querySelector(':has(picture');
+    else contentDiv.appendChild(div);
+  });
 
-    const span = document.createElement('span');
-    span.classList.add('seperator');
-    contentDiv.insertBefore(span, contentDiv.querySelector('div:nth-child(2)'));
-
-    block.appendChild(contentDiv);
-    picture.parentNode.classList.add('image');
-  }
+  /**
+   * define separator
+   */
+  const span = document.createElement('span');
+  span.classList.add('separator');
+  contentDiv.insertBefore(span, contentDiv.querySelector('div:nth-child(2)'));
+  block.appendChild(contentDiv);
+  picture.parentNode.classList.add('image');
 }
